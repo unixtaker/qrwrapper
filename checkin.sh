165a8cc228be88acd36d3eb35c142c74102ee745
unknown <jkessler@.er.erealty.com>
2011-03-09 11:54:34 -0600

    added git checkin script, that changes version numbers.

---
+++ checkin.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git add .
+# if git doesnt find any changes, then dont bother checking in
+if [ $? == 1 ]; then
+   echo "No changes found"
+   exit 1
+fi
+
+# change the app version
+oldversion=`grep -e "^version:.*" app.yaml | sed -e "s/version: //"`
+newversion=`expr $oldversion + 1`
+echo Changing version number to $newversion
+
+sed -i -e "s/^version:.*/version: $newversion/" app.yaml
+
+
+
+git commit
+if [ $? eq 0 ];
+git push
+fi
+
